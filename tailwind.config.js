47deacbdd725a888af0ae6c994952fd630cda442
omar6685 <omr6685@hotmail.com>
2023-03-25 22:54:03 +0300

    test

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      xs: '300px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      crd:'1024',
+      sc: '1070px',
+      nav2: '1180px',
+      nav:'1318px',
+      xl: '1440px'
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
